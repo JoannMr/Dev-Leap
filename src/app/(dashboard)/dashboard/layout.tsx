ebf6940443cb54cfee4ce8ca7694b3b13b1b8d39
JoannMr <145122335+JoannMr@users.noreply.github.com>
2025-04-09 19:42:13 +0200

    layout actualizada

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -2,6 +2,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import DashboardHeader from "@components/dashboard/DashboardHeader";
 import DashboardSidebar from "@components/dashboard/DashboardSidebar"; 
+import { SearchProvider } from "@/context/SearchContext";
 import "@/app/globals.css";
 
 export default function DashboardLayout({
@@ -13,13 +14,15 @@ export default function DashboardLayout({
     <html lang="en">
       <ClerkProvider>
         <body className="min-h-screen flex flex-col">
-          <DashboardHeader />
-          <div className="flex flex-1">
-            <DashboardSidebar />
-            <main className="flex-1 p-6 bg-white overflow-auto">
-              {children}
-            </main>
-          </div>
+          <SearchProvider>
+            <DashboardHeader />
+            <div className="flex flex-1">
+              <DashboardSidebar />
+              <main className="flex-1 p-6 bg-white overflow-auto">
+                {children}
+              </main>
+            </div>
+          </SearchProvider>
         </body>
       </ClerkProvider>
     </html>
